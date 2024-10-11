3c2ce1219aaa1c62309026ccf00195c81d254c11
rmohitzz21 <rmohit21264@gmail.com>
2024-10-11 11:21:59 +0530

    Ok

---
+++ d3_p1.c
@@ -0,0 +1,12 @@
+// 1. Write a C program to print student’s information like Name, Address, Mobile Number, email in separate line.
+
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+    printf("Rana Mohit\n");
+    printf("Ahmedabad\n");
+    printf("7621048017\n");
+
+getch();
+}
