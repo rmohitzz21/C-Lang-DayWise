209502c41c73481dd93ff754d0effd2613b59fbd
rmohitzz21 <rmohit21264@gmail.com>
2024-10-11 11:25:34 +0530

    Day 3 p-2

---
+++ d3_p2.c
@@ -0,0 +1,12 @@
+
+// 2. Write a C program to print student information in the centre of screen using various design pattern with help of backslash character (“\n”, “\t”)
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+    printf("\t\t\t\tRana Mohit\n");
+    printf("\t\t\t\tAhmedabad\n");
+    printf("\t\t\t\t7621048017\n");
+
+getch();
+}
